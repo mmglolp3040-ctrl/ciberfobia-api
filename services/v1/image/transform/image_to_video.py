2e5281872f35ada90dafa3009a2a71a3d8b7584f
mmglolp3040-ctrl <mmglolp3040@gmail.com>
2025-08-16 09:44:50 -0400

    Update image_to_video.py

--- services/v1/image/transform/image_to_video.py
+++ services/v1/image/transform/image_to_video.py
@@ -24,10 +24,10 @@ def process_image_to_video(image_url, length, frame_rate, zoom_speed, job_id, we
         # Determine orientation and set appropriate dimensions
         if width > height:
             scale_dims = "7680:4320"
-            output_dims = "1920x1080"
+            output_dims = "1024x1024"
         else:
             scale_dims = "4320:7680"
-            output_dims = "1080x1920"
+            output_dims = "1024x1024"
 
         # Calculate total frames and zoom factor
         total_frames = int(length * frame_rate)
